24bf129b4a612b8b8165c00127d74c7dc50347c0
Talha Naeem <talhanaeem347@gmail.com>
2023-02-14 02:30:51 +0500

    deployee

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+cd dist
+
+echo > .nojekyll
+
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+cd -
