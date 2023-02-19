8d0a521574c3c002b859ffa67478a01b38f68ed7
Talha Naeem <talhanaeem347@gmail.com>
2023-02-19 21:45:32 +0500

    hight adjested

--- src/views/Work.jsx
+++ src/views/Work.jsx
@@ -1,7 +1,7 @@
 import IFrame from "../components/IFrame";
 export default () => {
   return (
-    <section className="w-screen lg:h-screen md:h-screen h-full bg-blue-100 flex justify-center p-4 items-center lg:p-10 md:p-10">
+    <section className="w-screen lg:h-screen md:h- h-full  bg-blue-100 flex justify-center p-4 items-center lg:p-10 md:p-10">
       <div className="h-full w-full bg-red-100 rounded ">
         <h1 className="text-3xl font-bold text-center mt-2 " >Work</h1>
         <div className="flex flex-wrap items-stretch justify-around s/6  p-4  ">
@@ -14,7 +14,7 @@ export default () => {
           <div className="lg:h-2/5 md:h-1/2 h-40 lg:w-1/3 md:w-1/3 sm:w-2/3 w-full p-4 flex justify-center items-center  shadow-md">
             <IFrame img="/images/shoue.jpg" />
           </div>
-          <div className="lg:h-1/2 md:h-1/2 h-140 sm:w-2/3 w-full p-4 shadow-md">
+          <div className="lg:h-1/2 md:h-1/2 h-40 sm:w-2/3 w-full p-4 shadow-md">
             <IFrame img="/images/Portfolio.svg" />
           </div>
         </div>
