4f560627fcd59c0adb645691b3fada9e7a239541
Talha Naeem <75784944+talhanaeem347@users.noreply.github.com>
2023-02-20 11:01:29 +0500

    linkedin profile added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,20 @@ export default () => {
         <div className="bg-gray-600 h-12">
             <div className="p-2 w-full text-center flex justify-around items-center">
                 <p className="leading-normal"><i className="fa-regular fa-copyright"></i> Talha</p>
-                <span className="flex  ">
+                <div className="flex w-1/3 ">
                     <a className="bg-gray-200 border rounded-full p-1 px-3">
                     <i className="fa-brands fa-facebook-f fa-lg"></i>
                     </a>
-                    <a className="ml-4 bg-gray-200 border rounded-full p-1 px-2">
+                    <a className="px-2 bg-gray-200 border rounded-full p-1 px-2">
                     <i className="fa-brands fa-twitter fa-lg"></i>
                     </a>
-                    <a className="ml-4 bg-gray-200 border rounded-full p-1 px-2">
+                    <a className="px-2 bg-gray-200 border rounded-full p-1 px-2">
                     <i className="fa-brands fa-instagram fa-lg"></i>
                     </a>
-                    <a className="ml-4 bg-gray-200 border rounded-full p-1 px-2">
+                    <a href="www.linkedin.com/in/talhanaeem347" className="px-2 bg-gray-200 border rounded-full p-1 px-2">
                     <i className="fa-brands fa-linkedin-in fa-lg"></i>
                     </a>
-                </span>
+                </div>
             </div>
         </div>
     )
